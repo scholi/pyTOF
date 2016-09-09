a28395230a220b58a9a6947fa41a3bbe0557e19d
Scholder <olivier.scholder@empa.ch>
2016-09-09 14:40:19 +0200

    Add function to select image by mass or name. Also allow to shift the scans

--- ITA.py
+++ ITA.py
@@ -4,6 +4,7 @@ import struct
 import os.path
 import zlib
 import re
+import scipy
 
 class ITA:
 	def __init__(self, filename):
@@ -14,6 +15,10 @@ class ITA:
 		assert self.Type == b'ITStrF01'
 		self.root = Block.Block(self.f)
 		self.getMassInt()
+		self.sx = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.XSize').getLong()
+		self.sy = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.YSize').getLong()
+		self.Nscan = int(self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.NumberOfScans').getLong())
+		self.Nimg = int(self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.NumberOfImages').getLong())
 
 	def getMassInt(self):
 		R=[z for z in self.root.goto('MassIntervalList').getList() if z['name'].decode()=='mi']
@@ -48,26 +53,81 @@ class ITA:
 				p=self.peaks[P]
 				print(p[b'id']['long'],p[b'desc']['utf16'],p[b'assign']['utf16'],p[b'lmass']['float'],p[b'cmass']['float'],p[b'umass']['float'])
 
-	def getSize(self):
-		X = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.XSize').getLong()
-		Y = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.YSize').getLong()
-		return X,Y
+	def getSumImageByName(self, names, scans=None, **kargs):
+		if scans is None:
+			scans = range(self.Nscan)
+		if type(scans)==int: scans=[scans]
+		Z = np.zeros((self.sy,self.sx))
+		channels = self.getChannelsByName(names)
+		for s in scans:
+			for ch in channels:
+				ID = ch[b'id']['long']
+				Z+=self.getImage(ID,s,**kargs)
+		return Z,channels
 
-	def getNScans(self):
-		return self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.NumberOfScans').getLong()
+	def getShiftsByMass(self, masses, centered=True, prog=False):
+		Shifts=[(0,0)]
+		S0 = self.getSumImageByMass(masses,0)
+		Y = range(1,self.Nscan)
+		if prog:
+			from tqdm import tqdm
+			Y=tqdm(Y)
+		for i in Y:
+			S = self.getSumImageByMass(masses,i)
+			Shift = np.real( np.fft.fftshift( np.fft.ifft2( np.conj(np.fft.fft2(S0)) * np.fft.fft2(S) ) ) )
+			cord = np.unravel_index(np.argmax(Shift),S0.shape)
+			trans = (cord[1]-S0.shape[1]/2,cord[0]-S0.shape[0]/2)
+			Shifts.append(trans)
+		if centered:
+			avSx = np.round(np.mean([z[0] for z in Shifts]))
+			avSy = np.round(np.mean([z[1] for z in Shifts]))
+			Shifts = [(z[0]-avSx,z[1]-avSy) for z in Shifts]
+		return Shifts
+
+	def getXsectionByMass(self, x1, y1, x2, y2, masses, N=None, **kargs):
+		if N is None: N = np.sqrt((x2-x1)**2+(y2-y1)**2)
+		x=np.linspace(x1,x2,N)
+		y=np.linspace(y1,y2,N)
+		out=np.zeros((self.Nscan,N))
+		for s in range(self.Nscan):
+			Z = self.getSumImageByMass(masses,s,**kargs)
+			P = scipy.ndimage.map_coordinates(Z,np.vstack((y,x)))
+			out[s,:] = P
+		return out
 	
-	def getNImages(self):
-		Nimg = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.NumberOfImages').getLong()
+	def getSumImageByMass(self, masses, scans=None, **kargs):
+		if scans is None:
+			scans = range(self.Nscan)
+		if type(scans)==int: scans = [scans]
+		if type(masses)==int or type(masses) == float: masses=[masses]
+		Z = np.zeros((self.sy,self.sx))
+		for s in scans:
+			assert s>=0 and s<self.Nscan
+			for m in masses:
+				ch = self.getChannelByMass(m)
+				Z+=self.getImage(ch,s,**kargs)
+		return Z
 
-	def getImage(self, channel, scan):
+	def getImage(self, channel, scan, Shifts=None, ShiftMode='roll',**kargs):
 		assert type(channel) == int
 		assert type(scan) == int
-		Nscan = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.NumberOfScans').getLong()
-		Nimg = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image.NumberOfImages').getLong()
-		assert channel>=0 and channel<Nimg
-		assert scan>=0 and scan<Nscan
-		X,Y = self.getSize()
+		assert channel>=0 and channel<self.Nimg
+		assert scan>=0 and scan<self.Nscan
 		c = self.root.goto('filterdata/TofCorrection/ImageStack/Reduced Data/ImageStackScans/Image['+str(channel)+']/ImageArray.Long['+str(scan)+']')
 		D = zlib.decompress(c.value)
-		V = np.array(struct.unpack('<'+str(X*Y)+'I',D)).reshape((Y,X))
+		V = np.array(struct.unpack('<'+str(self.sx*self.sy)+'I',D),dtype=np.float).reshape((self.sy,self.sx))
+		if not Shifts is None:
+			r = [int(z) for z in Shifts[scan]]
+			V = np.roll(np.roll(V,-r[0],axis=1),-r[1],axis=0)
+			if ShiftMode=='const' or ShiftMode=='NaN':
+				if ShiftMode=='NaN': kargs['const']=np.nan
+				if 'const' not in kargs: raise KeyError('Missing argument const')
+				if r[1]<0:
+					V[:-r[1],:] = kargs['const']
+				elif r[1]>0:
+					V[-r[1]:,:] = kargs['const']
+				if r[0]<0:
+					V[:,:-r[0]] = kargs['const']
+				elif r[0]>0:
+					V[:,-r[0]:] = kargs['const']
 		return V
